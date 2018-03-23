2dd853b00fa927ce56c18baf6eb57661185df6fa
Monika Borawska <monikabor20@wp.pl>
2018-03-23 13:26:29 +0100

    Utworzenie slidera, kontaktu i mapy

---
+++ javascript/javascript.js
@@ -0,0 +1,39 @@
+ jQuery(document).ready(function ($) {
+
+     var slider_options = {
+         $AutoPlay: 1,
+         $Idle: 0,
+         $SlideDuration: 5000,
+         $SlideEasing: $Jease$.$Linear,
+         $PauseOnHover: 4,
+         $SlideWidth: 140,
+         $Align: 0
+     };
+
+     var slider_slider = new $JssorSlider$("slider", slider_options);
+
+     /*#region responsive code begin*/
+
+     var MAX_WIDTH = 980;
+
+     function ScaleSlider() {
+         var containerElement = slider_slider.$Elmt.parentNode;
+         var containerWidth = containerElement.clientWidth;
+
+         if (containerWidth) {
+
+             var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+             slider_slider.$ScaleWidth(expectedWidth);
+         } else {
+             window.setTimeout(ScaleSlider, 30);
+         }
+     }
+
+     ScaleSlider();
+
+     $(window).bind("load", ScaleSlider);
+     $(window).bind("resize", ScaleSlider);
+     $(window).bind("orientationchange", ScaleSlider);
+     /*#endregion responsive code end*/
+ });
