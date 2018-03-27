380ea315dfef40f177d9e9ddcba1e70f7cb46b7c
Monika Borawska <monikabor20@wp.pl>
2018-03-27 12:16:06 +0200

    Zmiany w stylowaniu, dodanie zdjec do karuzeli

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,40 +1,36 @@
 jQuery(document).ready(function ($) {
 
-     var slider_options = {
-         $AutoPlay: 1,
-         $Idle: 0,
-         $SlideDuration: 5000,
-         $SlideEasing: $Jease$.$Linear,
-         $PauseOnHover: 4,
-         $SlideWidth: 140,
-         $Align: 0
-     };
+    var slider_options = {
+        $AutoPlay: 1,
+        $Idle: 0,
+        $SlideDuration: 5000,
+        $SlideEasing: $Jease$.$Linear,
+        $PauseOnHover: 4,
+        $SlideWidth: 140,
+        $Align: 0
+    };
 
-     var slider_slider = new $JssorSlider$("slider", slider_options);
+    var slider_slider = new $JssorSlider$("slider", slider_options);
 
-     var MAX_WIDTH = 1400;
+    var MAX_WIDTH = 1400;
 
-     function ScaleSlider() {
-         var containerElement = slider_slider.$Elmt.parentNode;
-         var containerWidth = containerElement.clientWidth;
+    function ScaleSlider() {
+        var containerElement = slider_slider.$Elmt.parentNode;
+        var containerWidth = containerElement.clientWidth;
 
-         if (containerWidth) {
+        if (containerWidth) {
 
-             var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+            var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
 
-             slider_slider.$ScaleWidth(expectedWidth);
-         } else {
-             window.setTimeout(ScaleSlider, 30);
-         }
-     }
+            slider_slider.$ScaleWidth(expectedWidth);
+        } else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
 
-     ScaleSlider();
+    ScaleSlider();
 
-     $(window).bind("load", ScaleSlider);
-     $(window).bind("resize", ScaleSlider);
-     $(window).bind("orientationchange", ScaleSlider);
-     /*#endregion responsive code end*/
- });
-
-
-
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+});
